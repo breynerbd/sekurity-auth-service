4da76144a06192e8bf7fc728d3e7051ccf53ae78
DiegoLP1007 <dleiva-2021382@kinal.edu.gt>
2026-07-15 01:23:23 -0600

    Arreglo para calificar zonas en vista de usuarios

--- src/AuthService.Persistence/obj/Debug/net8.0/AuthService.Persistence.AssemblyInfo.cs
+++ src/AuthService.Persistence/obj/Debug/net8.0/AuthService.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb8c7dccab3373a4f8068c7fa35e700358c8c2e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea4e7d95f49f34af3cc836c9f2f1a88960d8ae2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
